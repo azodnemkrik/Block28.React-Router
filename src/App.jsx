c6236af040f4f8cd90f2f88ef87f7361646f105b
Kirk Mendoza <kirkmendoza@gmail.com>
2025-05-01 02:11:23 -0500

    Added Home component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import { Routes , Route , Link } from 'react-router-dom'
 import Blue from './components/Blue'
 import Red from './components/Red'
+import Home from './components/Home'
 
 function App() {
 
@@ -18,6 +19,7 @@ function App() {
 				<Routes>
 					<Route path="/blue" element={<Blue/>} />
 					<Route path="/red" element={<Red/>} />
+					<Route path="/" element={<Home/>} />
 				</Routes>
 			</div>
 		</div>
